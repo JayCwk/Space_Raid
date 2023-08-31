dbe44d05b76ee3d9fb696a9a9d8f46a5025994f0
KaiK1111 <141818407+KaiK1111@users.noreply.github.com>
2023-08-31 11:59:33 +0800

    Boss attack patern1 draft
    Draft only

---
+++ Space Raid/BossMovement/BossBullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEditorScript1 : MonoBehaviour
+{
+    public GameObject down;
+    public Transform bulletPos;
+
+    private float timer;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 2)
+        {
+            timer = 0;
+            shoot();
+
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(down, bulletPos.position, Quaternion.identity);
+    }
+}
