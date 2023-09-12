c01a96532fffbcccdb9dd81aa7541be1ac9b7bc4
CwY020212 <waiyau020212@gmail.com>
2023-09-12 23:20:47 +0800

    main menu complete

--- Space Raid/Assets/Scripts/MainMenu.cs
+++ Space Raid/Assets/Scripts/MainMenu.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public CanvasGroup optionPanel;
    public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(0);
+        SceneManager.LoadSceneAsync(1);
     }
+
 
     public void QuitGame()
     {
---
+++ Space Raid/Assets/Scripts/soundManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class soundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            load();
+        }
+        else
+        {
+            load();
+        }
+    }
+
+    public void changeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        save();
+    }
+
+
+    private void load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicvolume");
+    }
+
+    private void save()
+    {
+        PlayerPrefs.SetFloat("musicvolume", volumeSlider.value);
+    }
+}
