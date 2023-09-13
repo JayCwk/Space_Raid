8113cec950d8c19fcfe4e0d39745e880fd560397
JayCwk <cheahwk@1utar.my>
2023-09-13 17:00:48 +0800

    Added Enemies
    - added wall and explode mob
    - added wave spawner

---
+++ Space Raid/Assets/EnemyHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float health = 3;
+    public float damage = 1;
+
+    public void takeDamage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        health player = hitInfo.GetComponent<health>();
+        if (player != null)
+        {
+            player.TakeDamage(damage);
+        }
+    }
+}
